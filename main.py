3ea35a346e7f399faab95cca27ae292cf9a1c55c
ssbalajirao <balajiraoshanampudi@gmail.com>
2026-03-12 17:59:26 +0530

    initials commits-V2

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print ("hello world")
+import customtkinter as ct
+
+g_app = ct.CTk()
+
+# g_app title and geometry
+g_app.title("Music Tinker")
+g_app.geometry("800x800")
+
+
+g_button = ct.CTkButton(g_app, text = "Test button")
+
+g_app.mainloop()
